938a8711404f8b312224627c18af7d35a3c5b535
Ricardo Shiota Yasuda <shadowmaru@users.noreply.github.com>
2019-07-30 20:06:57 -0300

    Adiciona CMD do Flowclimate

--- config.ru
+++ config.ru
@@ -27,6 +27,6 @@ map Sinatra::Application.assets_prefix do
   run Sinatra::Application.sprockets
 end
 
-set :routes_to_cycle_through, %i[apoio-jira apoio-semaphore apoio-honeybadger]
+set :routes_to_cycle_through, %i[apoio-jira apoio-semaphore apoio-honeybadger apoio-flowclimate]
 
 run Sinatra::Application
---
+++ jobs/flowclimate.rb
@@ -0,0 +1,39 @@
+require 'net/http'
+require 'json'
+
+def flowclimate_cmd(options = {})
+  api_token = ENV['FLOWCLIMATE_API_TOKEN']
+  team_id = options[:team_id]
+
+  url = ENV['FLOWCLIMATE_URL'] + "/teams/#{team_id}/average_demand_cost"
+
+  uri = URI.parse(url)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = false
+  request = Net::HTTP::Get.new(
+    uri.request_uri,
+    {
+      'Accept' => 'application/json',
+      'Content-Type' => 'application/json',
+      'api_token' => api_token
+    }
+  )
+
+  response = http.request(request)
+
+  JSON.parse(response.body)
+end
+
+SCHEDULER.every '60m', first_in: 0 do |_job|
+  cmd = flowclimate_cmd(team_id: ENV['FLOWCLIMATE_TEAM_ID'])
+
+  send_event('team-name', title: cmd['data']['team_name'], text: cmd['message'])
+
+  send_event(
+    'cmdCurrent',
+    title: 'Semana Atual',
+    current: cmd['data']['current_week'],
+    last: cmd['data']['last_week'],
+  )
+  send_event('cmdLast', title: 'Última Semana', current: cmd['data']['last_week'])
+end
